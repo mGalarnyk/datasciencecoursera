e4e57e660a0cda6c358fbe42a574f41ba3ae9285
mGalarnyk <mgalarny@gmail.com>
2017-01-21 23:40:56 -0800

    Twitter API R

---
+++ 3_Getting_and_Cleaning_Data/AccessingDataTwitterAPI.R
@@ -0,0 +1,18 @@
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("twitteR")
+library(twitteR)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
+
+
+consumer_key <- "OQMbUsBfWQ1mVUGASpSArbG33"
+consumer_secret <- "GQ5kc0BlwJZE2FYyvv8cxn845z32ES6HsID87cawkQ075jwyIy"
+access_token <- 	"4338966852-lBmLvEg9mADHIdjK2hT4W5mtHmI9jRKxcV4PTrB"
+access_secret <- "AwKRZw9AvTMvMrb2jouX5JHTjDASI3zeceVsemgQa1SSq"
+
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+tw = twitteR::searchTwitter('#realDonaldTrump + #HillaryClinton', n = 1e4, since = '2016-11-08', retryOnRateLimit = 1e3)
+d = twitteR::twListToDF(tw)
--- 3_Getting_and_Cleaning_Data/quiz_week2_datatable.R
+++ 3_Getting_and_Cleaning_Data/quiz_week2_datatable.R
@@ -1,4 +1,7 @@
-# 1
+# 1. Register an application with the Github API here https://github.com/settings/applications. 
+# Access the API to get information on your instructors repositories (hint: this is the url you want "https://api.github.com/users/jtleek/repos").
+# Use this data to find the time that the datasharing repo was created. What time was it created?
+# see https://medium.com/@GalarnykMichael/accessing-data-from-github-api-using-r-3633fb62cb08#.z0z07ph5h for more details.
 
 #install.packages("jsonlite")
 library(jsonlite)
@@ -34,4 +37,10 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 # Subset data.frame
 gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
 
-# 2
+# 2. The sqldf package allows for execution of SQL commands on R data frames. We will use the sqldf package to practice the queries we might send with the dbSendQuery command in RMySQL.
+
+# Download the American Community Survey data and load it into an R object called acs
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
+# Which of the following commands will select only the data for the probability weights pwgtp1 with ages less than 50?
+
+
