0ce74cd32ab7cef4f2c038a964591d529acc0274
mGalarnyk <mgalarny@gmail.com>
2017-01-24 20:17:18 -0800

    video

--- 3_Getting_and_Cleaning_Data/AccessingDataTwitterAPI.R
+++ 3_Getting_and_Cleaning_Data/AccessingDataTwitterAPI.R
@@ -1,10 +1,10 @@
 #install.packages("twitteR")
 library(twitteR)
 
-consumer_key <- "OQMbUsBfWQ1mVUGASpSArbG33"
-consumer_secret <- "GQ5kc0BlwJZE2FYyvv8cxn845z32ES6HsID87cawkQ075jwyIy"
-access_token <- 	"4338966852-lBmLvEg9mADHIdjK2hT4W5mtHmI9jRKxcV4PTrB"
-access_secret <- "AwKRZw9AvTMvMrb2jouX5JHTjDASI3zeceVsemgQa1SSq"
+consumer_key <- "ehGQ5wrl51A5eHVPa7P2eObBn"
+consumer_secret <- "3gUna1oxe3jeorn5ai0cQh0vutSlIEl246KAH4tFVh29IopQbR"
+access_token <- 	"4338966852-S1rKY6euReyxBcAkDN24XOvBpAPVJZWx6KkJPqn"
+access_secret <- "uWFS7tQ9TIS4dipNNe6jSf3SAPve8BOobTghvJcBq5B5t"
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 tw = twitteR::searchTwitter('#realDonaldTrump + #HillaryClinton', n = 1e4, since = '2016-11-08', retryOnRateLimit = 1e3)
--- 3_Getting_and_Cleaning_Data/quiz_week2_datatable.R
+++ 3_Getting_and_Cleaning_Data/quiz_week2_datatable.R
@@ -43,4 +43,46 @@ gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 # https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
 # Which of the following commands will select only the data for the probability weights pwgtp1 with ages less than 50?
 
+# install.packages("sqldf")
+library("sqldf")
 
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+f <- file.path(getwd(), "ss06pid.csv")
+download.file(url, f)
+acs <- data.table::data.table(read.csv(f))
+query1 <- sqldf("select pwgtp1 from acs where AGEP < 50")
+
+# 3. Using the same data frame you created in the previous problem, 
+# what is the equivalent function to unique(acs$AGEP)
+
+sqldf("select distinct AGEP from acs")
+
+# 4. How many characters are in the 10th, 20th, 30th and 100th lines of HTML from this page:
+
+#http://biostat.jhsph.edu/~jleek/contact.html
+
+# (Hint: the nchar() function in R may be helpful)
+
+# connection <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+# htmlCode <- readLines(connection)
+# close(connection)
+# c(nchar(htmlCode[10]), nchar(htmlCode[20]), nchar(htmlCode[30]), nchar(htmlCode[100]))
+
+
+# 5. Read this data set into R and report the sum of the numbers in the fourth of the nine columns.
+
+# https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for
+
+# Original source of the data: http://www.cpc.ncep.noaa.gov/data/indices/wksst8110.for
+
+# (Hint this is a fixed width file format)
+
+#url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+# lines <- readLines(url, n = 10)
+# w <- c(1, 9, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3)
+# colNames <- c("filler", "week", "filler", "sstNino12", "filler", "sstaNino12", 
+#              "filler", "sstNino3", "filler", "sstaNino3", "filler", "sstNino34", "filler", 
+#              "sstaNino34", "filler", "sstNino4", "filler", "sstaNino4")
+#d <- read.fwf(url, w, header = FALSE, skip = 4, col.names = colNames)
+#d <- d[, grep("^[^filler]", names(d))]
+# sum(d[, 4])
