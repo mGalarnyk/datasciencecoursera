878868c28cbc86301e43d5bc1b7d9e9f93a534d0
Michael Galarnyk <mgalarny@gmail.com>
2017-03-03 15:58:40 -0800

    Add files via upload

--- 4_Exploratory_Data_Analysis/project2/plot1.R
+++ 4_Exploratory_Data_Analysis/project2/plot1.R
@@ -1,25 +1,18 @@
 library("data.table")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
 
-setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
-
-#Reads in data from file then subsets data for specified dates
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?"
-                             )
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
 # Prevents histogram from printing in scientific notation
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
 
-# Change Date Column to Date Type
-powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
 
-# Filter Dates for 2007-02-01 and 2007-02-02
-powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
-
-png("plot1.png", width=480, height=480)
-
-## Plot 1
-hist(powerDT[, Global_active_power], main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-
-dev.off()
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
