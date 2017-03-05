8a2696d50272ed3a94b4ec2f0588d2e317c99ff1
Michael Galarnyk <mgalarny@gmail.com>
2017-03-04 20:35:04 -0800

    Add files via upload

---
+++ 4_Exploratory_Data_Analysis/project2/plot3.R
@@ -0,0 +1,26 @@
+library("data.table")
+library("ggplot2")
+
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project2")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+# Load the NEI & SCC data frames.
+NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
+SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
+
+# Subset NEI data by Baltimore
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
