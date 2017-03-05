93dbadaa5f7865c54aa93df54037f27c6b506f4d
Michael Galarnyk <mgalarny@gmail.com>
2017-03-05 02:02:30 -0800

    Add files via upload

---
+++ 4_Exploratory_Data_Analysis/project2/plot6.R
@@ -0,0 +1,33 @@
+library("data.table")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+# Gather the subset of the NEI data which corresponds to vehicles
+condition <- grepl("vehicle", SCC[, SCC.Level.Two], ignore.case=TRUE)
+vehiclesSCC <- SCC[condition, SCC]
+vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data by each city's fip and add city name.
+vehiclesBaltimoreNEI <- vehiclesNEI[fips == "24510",]
+vehiclesBaltimoreNEI[, city := c("Baltimore City")]
+
+vehiclesLANEI <- vehiclesNEI[fips == "06037",]
+vehiclesLANEI[, city := c("Los Angeles")]
+
+# Combine data.tables into one data.table
+bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+
+png("plot6.png")
+
+ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+  facet_grid(scales="free", space="free", .~city) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+
+dev.off()
