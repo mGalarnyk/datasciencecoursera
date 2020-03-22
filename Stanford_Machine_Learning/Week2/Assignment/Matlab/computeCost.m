d8b4ef3294d531ca222a7af38b381c417e828847
Advay Aggarwal <advayaggarwal0301@gmail.com>
2020-03-22 18:39:24 +0530

    Update computeCost.m
    Completed the code

--- Stanford_Machine_Learning/Week2/Assignment/Matlab/computeCost.m
+++ Stanford_Machine_Learning/Week2/Assignment/Matlab/computeCost.m
@@ -12,7 +12,7 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+J = (1/(2*m)) *sum( (((X*theta)-y).^2))
 
 
 
